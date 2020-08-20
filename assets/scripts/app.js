989f973d5ed00beba7a559b2c098e7ca65930478
tropssap <57821127+tropssap@users.noreply.github.com>
2020-08-20 11:13:08 +0600

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,44 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWrideOutput(operator, resultBeforeCalc, calcNumber) {
+  const calculationDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calculationDescription);
+}
+
+function add() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWrideOutput("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWrideOutput("-", initialResult, enteredNumber);
+}
+
+function multiuply() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWrideOutput("*", initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWrideOutput("/", initialResult, enteredNumber);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiuply);
+divideBtn.addEventListener("click", divide);
